a26ca11e4a32009fdfbaaea3f5bf54f0df9daee1
Zahid Ahmedov <zahidahmadoffff@gmail.com>
2026-05-24 23:39:59 +0400

    add supabase fallback for vercel build

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ''
-)
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  'https://hwbylvuwmtvnesvqytae.supabase.co'
+
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3YnlsdnV3bXR2bmVzdnF5dGFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1NDE1OTEsImV4cCI6MjA5NTExNzU5MX0.r5trG_N6y06Ro_VbnywsDbLallCvT-PVTzq2fZWXkL8'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
